6c3897ab99a5dfebbe41ab9e8bba44406ea8dc7d
Mustafa Ozbalci <mustafa.ozbalci@etiya.com>
2024-08-12 15:28:45 +0300

    Unique logId added to response header.

--- src/main/java/demo/Wallet/filter/Slf4jMDCFilter.java
+++ src/main/java/demo/Wallet/filter/Slf4jMDCFilter.java
@@ -17,13 +17,15 @@ import java.util.UUID;
 public class Slf4jMDCFilter extends OncePerRequestFilter {
 
     private static final String MDC_UUID_TOKEN_KEY = "Slf4jMDCFilter.UUID";
+    private static final String UNIQUE_ID_HEADER_NAME = "Unique-logId";
     private static final String ERROR_FORMAT = "Exception occurred in filter while setting UUID for logs: {}";
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws ServletException, IOException {
+        String uniqueId = UUID.randomUUID().toString();
         try {
-            MDC.put(MDC_UUID_TOKEN_KEY, UUID.randomUUID().toString());
-            chain.doFilter(request, response);
+            MDC.put(MDC_UUID_TOKEN_KEY, uniqueId);
+            response.addHeader(UNIQUE_ID_HEADER_NAME, uniqueId);
         } catch (Exception ex) {
             log.error(ERROR_FORMAT, ex.getMessage(), ex);
         } finally {
